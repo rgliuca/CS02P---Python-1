ca24db2b603c4a8121fc60cc95bb4c8af3e36124
rgliuca <44307228+rgliuca@users.noreply.github.com>
2019-04-01 12:37:16 -0700

    Add files via upload

---
+++ parse for integers corner cases.py
@@ -0,0 +1,13 @@
+# corner cases:
+#   There are 16.1 candles in the birthday cake for his 88th birthday.
+#   The answer for 2 plus 5 is 7.
+#   The number 10e100 is huge, but the number 1e0 is not and so is 50.
+#   How about these numbers: -100, 90.2 and 77.
+#   21bottles of beer on the wall21 bottles of beer on the wall. 21 bottles of beer on the wall
+#   21 bottles of beer on the wall.21 bottles of beer on the wall
+#   The number 10e is weird, but not the numbers 1e0 and 50.
+#
+# not covered cases:
+#   21e+ is another weird number, but it's not covered, will be skipped as an int.
+
+
