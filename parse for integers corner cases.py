cc161d387c234b2953c800625efac008a8e912b7
rgliuca <44307228+rgliuca@users.noreply.github.com>
2020-10-23 05:42:19 -0700

    Update parse for integers corner cases.py

--- parse for integers corner cases.py
+++ parse for integers corner cases.py
@@ -1,7 +1,7 @@
 # corner cases:
 #   There are 16.1 candles in the birthday cake for his 88th birthday.
 #   The answer for 2 plus 5 is 7.
-#   The number 10e100 is huge, but the number 1e0 is not and so is 50.
+#   The number 10e100 is huge, but the number 1e0 is not and neither is 50.
 #   How about these numbers: -100, 90.2 and 77.
 #   21bottles of beer on the wall21 bottles of beer on the wall. 21 bottles of beer on the wall
 #   21 bottles of beer on the wall.21 bottles of beer on the wall
